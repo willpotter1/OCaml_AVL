152237391a223d60ad9109f2d35d1d4b5bca519f
willpotter1 <willfspot@yahoo.co.uk>
2024-11-18 11:46:05 -0500

    Changed find to not use extra bound n unnecessarily. Added some context to readme file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# OCaml_AVL
+Implementing AVL
+
+Functions:
+-find
+-insert/delete
+    -find
+    -rotate right, rotate left
+-sort
+    -insert
+    -in order traversal
+
+
+Current update:
+
+find completed
+    -input: binary tree and key
+    -return: value assoc to key or raises lookup_error of key if not found
--- find.ml
+++ find.ml
@@ -5,18 +5,14 @@ type ’a btree =
 	| Node of ’a btree * ( key * ’a ) * ’a btree
 
 type lookup_error =
-  | BoundExceeded
   | NoSuchKey of key
 
 exception LookupError of lookup_error
 
-let rec findEx'' t k n = 
-  match t with
-  | _ when n < 0 -> lookup_error BoundExceeded
-  | Empty -> lookup_error (NoSuchKey k)
-  | Node (l, (k', _), _) when k < k' -> findEx' l k (n - 1)
-  | Node (_, (k', _), r) when k > k' -> findEx' r k (n - 1)
-  | Node (_, (_,  x), _) -> x
+let rec find t k = match t with 
+  | Empty -> raise lookup_error (NoSuchKey k) (* raise instead of using None *)
+  | Node (l, (k', d), r) when k < k' -> find l k
+  | Node (_, (k', _), r) when k > k' -> find r k
+  | Node (_, (_,  x), _) -> x (* no need to use Some now *)
 
-  
 
